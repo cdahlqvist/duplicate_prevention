4bc1b37c672c621d6c81dd7ef6040c1111e39722
Christian Dahlqvist <christian@elastic.com>
2016-02-14 15:31:09 +0000

    Updated to use available plugins for ID generation.

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 logstash_path='./bin/logstash'
 
-# Index 5.0M logs at a time up to 150M for each of threee configurations
-iterations=30
+# Index 5.0M logs at a time up to 200M for each of threee configurations
+iterations=40
 log_count=5000000
 workers=8
 
@@ -18,20 +18,20 @@ do
     curl -XGET "http://$host:9200/_cat/indices" | grep auto >> ./log_autoid.log
 
     start_time=`date +%s`
-    ./cat_logs logs $log_count | $logstash_path -w $workers -f ./hashid1.conf
+    ./cat_logs logs $log_count | $logstash_path -w $workers -f ./uuid.conf
     end_time=`date +%s`
     duration=`expr $end_time - $start_time`
-    echo `date` " => $log_count records processed with SHA1 hash based ID in $duration seconds." >> ./log_hashid1.log
+    echo `date` " => $log_count records processed with UUID based ID in $duration seconds." >> ./log_uuid.log
 
     sleep 5
-    curl -XGET "http://$host:9200/_cat/indices" | grep hash1 >> ./log_hashid1.log
+    curl -XGET "http://$host:9200/_cat/indices" | grep hash1 >> ./log_uuid.log
 
     start_time=`date +%s`
-    ./cat_logs logs $log_count | $logstash_path -w $workers -f ./hashid2.conf
+    ./cat_logs logs $log_count | $logstash_path -w $workers -f ./hashid.conf
     end_time=`date +%s`
     duration=`expr $end_time - $start_time`
-    echo `date` " => $log_count records processed with time and MD5 hash based ID in $duration seconds." >> ./log_hashid2.log
+    echo `date` " => $log_count records processed with time and 96-bit MD5 hash based ID in $duration seconds." >> ./log_hashid.log
 
     sleep 5
-    curl -XGET "http://$host:9200/_cat/indices" | grep hash2 >> ./log_hashid2.log
+    curl -XGET "http://$host:9200/_cat/indices" | grep hash2 >> ./log_hashid.log
 done
